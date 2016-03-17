9ecedbd6cd10b85c2132274b7961d2dbf21d2454
Yu Mao <ymao1@latedays.andrew.cmu.edu>
2016-03-17 01:11:46 -0400

    Implement client request queue. Implement multiple worker nodes.

--- assignment4/src/myserver/master.cpp
+++ assignment4/src/myserver/master.cpp
@@ -4,22 +4,56 @@
 
 #include "server/messages.h"
 #include "server/master.h"
+#include <queue>
+#include <map>
 
+/**
+ * Request_info records the request's info
+ */
+typedef struct _Request_info{
+  Client_handle client_handle;
+  Request_msg client_req;
 
+  _Request_info(Client_handle client_handle, Request_msg client_req):
+  client_handle(client_handle),
+  client_req(client_req){}
+
+} Request_info;
+
+/**
+ * the state of worker
+ */
+typedef struct _Worker_state{
+  Worker_handle worker_handle;
+  bool is_busy;
+} Worker_state;
+
+/**
+ * the state of the master
+ */
 static struct Master_state {
 
-  // The mstate struct collects all the master node state into one
-  // place.  You do not need to preserve any of the fields below, they
-  // exist only to implement the basic functionality of the starter
-  // code.
-
-  bool server_ready;
-  int max_num_workers;
-  int num_pending_client_requests;
+  //tag counter
   int next_tag;
 
-  Worker_handle my_worker;
-  Client_handle waiting_client;
+  //is the server ready to go?
+  bool server_ready;
+  
+  //max number of workers configured
+  int max_num_workers;
+
+  //the number of all pending requests:
+  //pending requests =request being processed + requests in the queue
+  int num_pending_client_requests;
+
+  //workers
+  std::vector<Worker_state> my_workers;
+
+  //request queue
+  std::queue<Request_info> requests_queue;
+
+  //worker client map
+  std::map<Worker_handle, Client_handle> workerClientMap;
 
 } mstate;
 
@@ -40,12 +74,15 @@ void master_node_init(int max_workers, int& tick_period) {
   // when 'master_node_init' returnes
   mstate.server_ready = false;
 
-  // fire off a request for a new worker
-
-  int tag = random();
-  Request_msg req(tag);
-  req.set_arg("name", "my worker 0");
-  request_new_worker_node(req);
+  // fire off new workers
+  int initNumWorker = max_workers;
+  for(int i = 0; i<initNumWorker; i++)
+  {
+    int tag = random();
+    Request_msg req(tag);
+    req.set_arg("name", "my worker " + i);
+    request_new_worker_node(req);   
+  }
 
 }
 
@@ -54,8 +91,10 @@ void handle_new_worker_online(Worker_handle worker_handle, int tag) {
   // 'tag' allows you to identify which worker request this response
   // corresponds to.  Since the starter code only sends off one new
   // worker request, we don't use it here.
-
-  mstate.my_worker = worker_handle;
+  Worker_state state;
+  state.worker_handle = worker_handle;
+  state.is_busy = false;
+  mstate.my_workers.push_back(state);
 
   // Now that a worker is booted, let the system know the server is
   // ready to begin handling client requests.  The test harness will
@@ -73,9 +112,41 @@ void handle_worker_response(Worker_handle worker_handle, const Response_msg& res
 
   DLOG(INFO) << "Master received a response from a worker: [" << resp.get_tag() << ":" << resp.get_response() << "]" << std::endl;
 
-  send_client_response(mstate.waiting_client, resp);
+  Client_handle client = mstate.workerClientMap[worker_handle];
+  send_client_response(client, resp);
+  mstate.num_pending_client_requests --;
 
-  mstate.num_pending_client_requests = 0;
+  //find the worker
+  for(unsigned int i=0; i< mstate.my_workers.size(); i++)
+  {
+    if(mstate.my_workers[i].worker_handle == worker_handle)
+    {
+      //mark the worker as idle
+      mstate.my_workers[i].is_busy = false;
+      
+      //if the request queue is not null
+      //execute the next request.
+      if(!mstate.requests_queue.empty())
+      {
+        Worker_handle worker_handle = mstate.my_workers[i].worker_handle;
+        Request_info req = mstate.requests_queue.front();
+        
+        DLOG(INFO) << "deque:" << req.client_req.get_request_string()<< std::endl;
+        
+        mstate.requests_queue.pop();
+        int tag = mstate.next_tag++;
+        Request_msg worker_req(tag, req.client_req);
+        mstate.workerClientMap[worker_handle] = req.client_handle;
+        mstate.my_workers[i].is_busy = true; //mark the worker as busy
+        send_request_to_worker(worker_handle, req.client_req);
+      }
+      
+      break;
+    }
+  }
+
+  return;
+
 }
 
 void handle_client_request(Client_handle client_handle, const Request_msg& client_req) {
@@ -92,35 +163,38 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
     return;
   }
 
-  // The provided starter code cannot handle multiple pending client
-  // requests.  The server returns an error message, and the checker
-  // will mark the response as "incorrect"
-  if (mstate.num_pending_client_requests > 0) {
-    Response_msg resp(0);
-    resp.set_response("Oh no! This server cannot handle multiple outstanding requests!");
-    send_client_response(client_handle, resp);
-    return;
-  }
-
-  // Save off the handle to the client that is expecting a response.
-  // The master needs to do this it can response to this client later
-  // when 'handle_worker_response' is called.
-  mstate.waiting_client = client_handle;
   mstate.num_pending_client_requests++;
 
-  // Fire off the request to the worker.  Eventually the worker will
-  // respond, and your 'handle_worker_response' event handler will be
-  // called to forward the worker's response back to the server.
-  int tag = mstate.next_tag++;
-  Request_msg worker_req(tag, client_req);
-  send_request_to_worker(mstate.my_worker, worker_req);
+  //assign the work to the worker that is not busy
+  bool is_assigned = false;
+  for(unsigned int i=0; i<mstate.my_workers.size(); i++)
+  {
+    if(!mstate.my_workers[i].is_busy)
+    {
+      Worker_handle worker_handle = mstate.my_workers[i].worker_handle;
+      int tag = mstate.next_tag++;
+      Request_msg worker_req(tag, client_req);
+      mstate.workerClientMap[worker_handle] = client_handle;
+      mstate.my_workers[i].is_busy = true; //mark the worker as busy
+      send_request_to_worker(worker_handle, worker_req);
+      is_assigned = true;
+      break;
+    }
+  }
+
+  //if all workers are busy, push the request to queue
+  if(!is_assigned)
+  {
+    DLOG(INFO) << "enque:" << client_req.get_tag() << ":" << client_req.get_request_string() << std::endl;
+    mstate.requests_queue.push(Request_info(client_handle, client_req));
+  }
 
   // We're done!  This event handler now returns, and the master
   // process calls another one of your handlers when action is
   // required.
+  return;
 
 }
-
 
 void handle_tick() {
 

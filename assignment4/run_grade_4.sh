6e4f8213e3449008327cbefaf30277e9ad2fa8ce
Che-Yuan Liang <cheyuanl@latedays.andrew.cmu.edu>
2016-03-19 13:44:24 -0400

    script for auto run the grading

---
+++ assignment4/run_grade_4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./run_latedays.sh 4 tests/grading_wisdom.txt
+./run_latedays.sh 4 tests/grading_compareprimes.txt
+./run_latedays.sh 4 tests/grading_tellmenow.txt
+./run_latedays.sh 4 tests/grading_nonuniform1.txt
+./run_latedays.sh 4 tests/grading_nonuniform2.txt
+./run_latedays.sh 4 tests/grading_nonuniform3.txt
+./run_latedays.sh 4 tests/grading_uniform1.txt
